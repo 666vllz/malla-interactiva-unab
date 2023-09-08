4e6f85164c96f8f658acbffb75f3804556d38daa
Luis Torres <luis.i6nacio@gmail.com>
2023-09-08 19:23:13 -0300

    temporal course item component

--- frontend/src/components/CourseItemTest.jsx
+++ frontend/src/components/CourseItemTest.jsx
@@ -1,9 +1,47 @@
 import CourseInfo from "./CourseInfo"
 import CourseRequirements from "./CourseRequirements"
 
-const CourseItemTest = ({ code, courseName, credits, requirements }) => {
+const CourseItemTest = ({
+  code,
+  courseName,
+  credits,
+  requirements,
+  hoveredCourseRequirements,
+  setHoveredCourseRequirements,
+  hoveredCourseCode,
+  setHoveredCourseCode,
+}) => {
+  const isPrevRequirement = hoveredCourseRequirements.includes(code)
+  const isNextRequirement = requirements.includes(hoveredCourseCode)
+
+  const handleMouseOver = () => {
+    setHoveredCourseRequirements(requirements)
+    setHoveredCourseCode(code)
+  }
+
+  const handleMouseLeave = () => {
+    setHoveredCourseRequirements([])
+    setHoveredCourseCode(null)
+  }
+
+  const prevRequirementStyle = `bg-[rgba(255,255,255,0.8)] text-[#121212] transition-all duration-300 ease-in-out`
+
+  const nextRequirementStyles = `bg-[rgba(255,255,255,0.8)] text-[#121212] transition-all duration-300 ease-in-out`
+
+  const className = `inline-block w-32 rounded border border-white bg-[#202020] p-2 ${
+    isPrevRequirement
+      ? prevRequirementStyle
+      : isNextRequirement
+      ? nextRequirementStyles
+      : "border-opacity-20"
+  }`
+
   return (
-    <div className="inline-block w-32 rounded border border-white border-opacity-20 bg-[#202020] p-2">
+    <div
+      className={className}
+      onMouseOver={handleMouseOver}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="h-14">
         <h3 className="font-bold">{courseName}</h3>
       </div>

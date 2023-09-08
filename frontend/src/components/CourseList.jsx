23a4bb80004ebf5d079a4a8c5d357a114716a1c2
Luis Torres <luis.i6nacio@gmail.com>
2023-09-08 17:34:46 -0300

    adding horizontal scroll

--- frontend/src/components/CourseList.jsx
+++ frontend/src/components/CourseList.jsx
@@ -1,14 +1,8 @@
-/* eslint-disable react/prop-types */
 import CourseItemTest from "./CourseItemTest"
 
 const CourseList = ({ courses }) => {
   return (
-    <div
-      style={{
-        gridTemplateColumns: `repeat(${courses.length}, minmax(0, 1fr))`,
-      }}
-      className="grid gap-2"
-    >
+    <div className="container flex gap-2 overflow-x-scroll text-xs">
       {courses.map((semester, index) => (
         <div key={index} className="inline-flex flex-col gap-2">
           <h3 className="cursor-pointer rounded bg-[#202020] text-center hover:bg-[#303030]">

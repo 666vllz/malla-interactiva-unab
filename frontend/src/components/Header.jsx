9ca35c2c4078690316eef87386e7e966c0084ede
Luis Torres <luis.i6nacio@gmail.com>
2023-09-08 17:38:07 -0300

    removing styles

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="w-full border-b">
-      <div className="container flex h-16 items-center space-x-4">
+      <div className="container flex h-12 items-center">
         <h2 className="font-bold">Ingeniería Civil Informática</h2>
       </div>
     </header>

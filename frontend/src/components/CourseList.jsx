b2517e4d8b1decbe9adee63930901639bef6bdd1
Luis Torres <luis.i6nacio@gmail.com>
2023-09-12 01:45:52 -0300

    passing handleClick prop

--- frontend/src/components/CourseList.jsx
+++ frontend/src/components/CourseList.jsx
@@ -1,28 +1,31 @@
 import { useState } from "react"
-import CourseItemTest from "./CourseItemTest"
+import CourseItem from "./CourseItem"
 
-const CourseList = ({ courses }) => {
+const CourseList = ({ courses, handleCourseClick }) => {
   const [hoveredCourseRequirements, setHoveredCourseRequirements] = useState([])
   const [hoveredCourseCode, setHoveredCourseCode] = useState(null)
 
   return (
-    <div className="container flex gap-2 overflow-x-scroll text-xs">
+    <div className="flex gap-2 text-xs">
       {courses.map((semester, index) => (
         <div key={index} className="inline-flex flex-col gap-2">
           <h3 className="cursor-pointer rounded bg-[#202020] text-center hover:bg-[#303030]">
             Semestre {index + 1}
           </h3>
-          {semester.map((course, index) => (
-            <CourseItemTest
-              key={index}
+          {semester.map((course) => (
+            <CourseItem
+              key={course.id}
+              courseId={course.id}
               code={course.code}
               courseName={course.courseName}
               credits={course.credits}
               requirements={course.requirements}
+              approved={course.approved}
               hoveredCourseRequirements={hoveredCourseRequirements}
               setHoveredCourseRequirements={setHoveredCourseRequirements}
               hoveredCourseCode={hoveredCourseCode}
               setHoveredCourseCode={setHoveredCourseCode}
+              handleClick={handleCourseClick}
             />
           ))}
         </div>

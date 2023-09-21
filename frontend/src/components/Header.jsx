c29329b246f87c9ec33d47fdeeeeb2ca0da42139
Luis Torres <luis.i6nacio@gmail.com>
2023-09-21 00:03:45 -0300

    adding state to the select value in ComboBox

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,7 @@
-import ComboBox from "./ComboBox"
 import { careers } from "../data/careers"
+import { useState } from "react"
+import CareersComboBox from "./CareersComboBox"
+import YearComboBox from "./YearComboBox"
 
 const formatCareers = () => {
   // Create a dictionary to store merged data
@@ -23,17 +25,60 @@ const formatCareers = () => {
 }
 
 const Header = () => {
-  console.log(careers)
+  const [selectedCareer, setSelectedCareer] = useState("")
+  const [selectedProgram, setSelectedProgram] = useState("")
+  const [programOptions, setProgramOptions] = useState([])
+
   const formatedCareers = formatCareers()
 
-  console.log(formatedCareers)
+  const handleSelectedCareer = (careerCode) => {
+    const career = formatedCareers.filter(
+      (career) => career.PROGRAMA === careerCode
+    )
+
+    setSelectedCareer(career[0].PROGRAMA)
+    setProgramOptions(career[0].PERIODOS)
+  }
+
+  const handleSelectedProgram = (programCode) => {
+    setSelectedProgram(programCode)
+  }
+
+  const handleClick = () => {
+    alert(`codigo carrera: ${selectedCareer}\ncodigo malla: ${selectedProgram}`)
+  }
 
   return (
-    <header className="w-full border-b">
+    <header className="w-full border-b py-2">
       <div className="container flex h-12 items-center justify-between">
         <h2 className="font-bold">Ingeniería Civil Informática</h2>
-        <ComboBox options={formatedCareers} />
-        <ComboBox options={formatedCareers} />
+        <div className="flex gap-4">
+          {/* <ComboBox
+            defaultText="Seleccionar carrera"
+            notFoundText="Carrera no encontrada"
+            options={formatedCareers}
+            handleSelectedCareer={handleSelectedCareer}
+          /> */}
+          {/* <ComboBox
+            defaultText="Seleccionar malla"
+            notFoundText="Malla no encontrada"
+            options={formatedCareers}
+          /> */}
+          <CareersComboBox
+            options={formatedCareers}
+            handleSelectedCareer={handleSelectedCareer}
+          />
+          <YearComboBox
+            options={programOptions}
+            handleSelectedProgram={handleSelectedProgram}
+          />
+          <button
+            className="rounded border border-white bg-[#202020] px-4 hover:bg-zinc-300 hover:text-black"
+            onClick={handleClick}
+          >
+            Buscar
+          </button>
+        </div>
       </div>
     </header>
   )

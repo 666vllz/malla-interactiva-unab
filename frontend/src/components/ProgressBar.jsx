cb52c0280e6be65c7acb5a789ae5e86725c2759d
Luis Torres <luis.i6nacio@gmail.com>
2023-10-17 18:22:03 -0300

    adding progress value

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -1,7 +1,12 @@
 import { Progress } from "@/components/ui/progress"
 
 const ProgressBar = ({ progress }) => {
-  return <Progress value={progress} className="my-5 w-[100%]" />
+  return (
+    <div className="flex w-full flex-col">
+      <span className="font-bold">{progress}%</span>
+      <Progress value={progress} />
+    </div>
+  )
 }
 
 export default ProgressBar

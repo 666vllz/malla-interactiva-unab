9db633772b96bd7dc7d9a5c0ae886812a8d3a865
Luis Torres <luis.i6nacio@gmail.com>
2023-10-17 18:21:39 -0300

    changing size

--- frontend/src/components/InfoIcon.jsx
+++ frontend/src/components/InfoIcon.jsx
@@ -6,7 +6,7 @@ const InfoIcon = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="h-6 w-6"
+      className="h-5 w-5"
     >
       <path
         strokeLinecap="round"

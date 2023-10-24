d9eb1fca37b5c23e4c4570f9a96886b1b3bd206d
Luis Torres <luis.i6nacio@gmail.com>
2023-10-23 22:54:20 -0300

    using react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,4 +2,20 @@ import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Login from "./components/Login.jsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/malla",
+    element: <App />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+)
